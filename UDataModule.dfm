f662d445f311083779e6fdcc70672fcbda8d90e2
HBenzaoui <hamzapdd@gmail.com>
2022-06-12 03:32:08 +0100

    add Serial Number form

--- UDataModule.dfm
+++ UDataModule.dfm
@@ -4236,4 +4236,10 @@ object DataModuleF: TDataModuleF
     Left = 76
     Top = 272
   end
+  object FDQuery1: TFDQuery
+    FilterOptions = [foCaseInsensitive]
+    Connection = MainForm.GstockdcConnection
+    Left = 884
+    Top = 362
+  end
 end
---
+++ USNumberGestion.pas
@@ -0,0 +1,24 @@
+﻿unit USNumberGestion;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+
+type
+  TSerialNGestionF = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  SerialNGestionF: TSerialNGestionF;
+
+implementation
+
+{$R *.dfm}
+
+end.
